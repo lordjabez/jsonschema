e9a693795ab3d09112cbf30d25fea4640d2be04c
Judson D Neer <judson.neer@gmail.com>
2013-11-08 08:46:53 -0800

    Allow draft 3 style 'required' fields in draft 4 schemas.

--- jsonschema/_validators.py
+++ jsonschema/_validators.py
@@ -285,6 +285,17 @@ def properties_draft4(validator, properties, instance, schema):
                 schema_path=property,
             ):
                 yield error
+        elif subschema.get("required", False):
+            error = ValidationError("%r is a required property" % property)
+            error._set(
+                validator="required",
+                validator_value=subschema["required"],
+                instance=instance,
+                schema=schema,
+            )
+            error.path.appendleft(property)
+            error.schema_path.extend([property, "required"])
+            yield error
 
 
 def required_draft4(validator, required, instance, schema):
