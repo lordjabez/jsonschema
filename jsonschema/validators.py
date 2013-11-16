b89c2d08607d45fd79cbb5a2890ceb111f05e02d
Judson D Neer <jud.neer@ngc.com>
2013-11-16 12:24:35 -0800

    Revert "Allow loading of schemas from file instead of fetching from URL." The urllib already provides this capability.
    This reverts commit 949350446877784cc45d6fa3713fb22e9fe32045.

--- jsonschema/validators.py
+++ jsonschema/validators.py
@@ -3,8 +3,6 @@ from __future__ import division, unicode_literals
 import contextlib
 import json
 import numbers
-import os
-
 
 try:
     import requests
@@ -372,10 +370,8 @@ class RefResolver(object):
             else:
                 result = requests.get(uri).json
         else:
-
-            # Instead of trying to open a remote URI, we just load the schema from file.
-            with open(os.path.join('schemas', uri)) as s:
-                result = json.load(s)
+            # Otherwise, pass off to urllib and assume utf-8
+            result = json.loads(urlopen(uri).read().decode("utf-8"))
 
         if self.cache_remote:
             self.store[uri] = result
